b6c38e34cc20d73ed9543933c24ea6af3485f32c
galan <galandets@gmail.com>
2019-03-07 11:30:39 +0300

    Добавил пример робота

---
+++ __sample.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Пример создания роботов
+ */
+
+namespace {
+    // Глобальные инициализации
+}
+
+namespace bot {
+
+    use Galantcev\Components\Bot;
+
+    /**
+     * Пример робота
+     * Class SampleBot
+     * @package bot
+     */
+    class SampleBot extends Bot
+    {
+        /**
+         * @inheritdoc
+         */
+        public function __construct()
+        {
+            parent::__construct();
+
+            // Будем блокировать повторные запуски робота
+            $this->setupLock(__FILE__ . '.lock', 60 * 60 * 1);
+
+
+            // Устанавливаем коллбек в случае некорректного завершения робота
+            $this->start(function () {
+                $this->log->error('А вот тут у нас случилось прерывание, а ничего успешно не завершено!');
+
+                $this->deleteLockFile();
+            });
+
+        }
+
+        /**
+         * Робот что-нибудь делает
+         */
+        public function go()
+        {
+            // Можем ли мы вообще работать или должны сушить ласты
+            if (!$this->canWeWork()) {
+                $this->finish();
+
+                return;
+            }
+
+            $this->log->text('Текст');
+            $this->log->error('Ошибка!');
+            $this->log->warning('Внимание.');
+            $this->log->success('Успех :)');
+
+            // А здесь говорим, что на самом деле всё хорошо отработало
+            $this->finish();
+            $this->deleteLockFile();
+        }
+    }
+
+    $bot = new SampleBot();
+
+    $bot->log->text('Начинаем работу!');
+
+    $bot->go();
+
+    $bot->log->text('Завершаем работу.');
+}
